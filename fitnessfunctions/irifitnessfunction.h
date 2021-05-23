850e814a8c6120730d100c0c09fd95270873adf4
Andres Cienfuegos <andres.acienfuegos@gmail.com>
2021-05-23 11:06:45 +0200

    garbage fitness func

--- fitnessfunctions/irifitnessfunction.h
+++ fitnessfunctions/irifitnessfunction.h
@@ -1,5 +1,5 @@
-#ifndef IRIFITNESSFUNCTION_H_
-#define IRIFITNESSFUNCTION_H_
+#ifndef GARBAGEFITNESSFUNCTION_H_
+#define GARBAGEFITNESSFUNCTION_H_
 
 /******************************************************************************/
 /******************************************************************************/
@@ -10,27 +10,41 @@
 #include "general.h"
 using namespace std;
 
-class CIriFitnessFunction;
+class CGarbageFitnessFunction;
 
 #include "fitnessfunction.h"
 #include "simulator.h"
+#include "sensor.h"
+#include "batterysensor.h"
+#include "groundsensor.h"
+#include "groundmemorysensor.h"
+#include "lightsensor.h"
+#include "bluelightsensor.h"
+#include "contactsensor.h"
 
 /******************************************************************************/
 /******************************************************************************/
 
-class CIriFitnessFunction : public CFitnessFunction
+class CGarbageFitnessFunction : public CFitnessFunction
 {
 public:
-    CIriFitnessFunction(const char* pch_name, CSimulator* pc_simulator,
+    CGarbageFitnessFunction(const char* pch_name, CSimulator* pc_simulator,
                                     unsigned int un_collisions_allowed_per_epuck);
-		~CIriFitnessFunction();
+		~CGarbageFitnessFunction();
     virtual double GetFitness();
 		virtual void SimulationStep(unsigned int n_simulation_step, double f_time, double f_step_interval);
 
 protected:
 		unsigned int m_unNumberOfSteps;
 		double 			m_fComputedFitness;
+		unsigned int m_unState;
+
 		CEpuck* m_pcEpuck;
+		CBatterySensor* m_seBattery;
+		
+		unsigned int m_unCollisionsNumber;
+		unsigned int m_unGreyFlag;
+		unsigned int m_unGreyCounter;
 
 };
 
