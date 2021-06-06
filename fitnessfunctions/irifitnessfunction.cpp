6770eb3c99354d8c5b713acb09e80a2d69528697
Andres Cienfuegos <andres.acienfuegos@gmail.com>
2021-06-06 22:25:41 +0200

    clean up

--- fitnessfunctions/irifitnessfunction.cpp
+++ fitnessfunctions/irifitnessfunction.cpp
@@ -64,15 +64,7 @@ double CIriFitnessFunction::GetFitness()
 	/* If fitness less than 0, put it to 0 */
 	/* if ( fit < 0.0 ) fit = 0.0; */
 
-	/* exp1, exp3 */
   double fit = ( m_fComputedFitness / (double) m_unNumberOfSteps ) * (1 - ((double) (fmin(m_unCollisionsNumber,30.0)/30.0))) * ( (double) (fmin(m_unGreyCounter, 5.0)/ 5.0 ));
-
-	/* exp2, exp4 */
-  /* double fit = ( m_fComputedFitness / (double) m_unNumberOfSteps ) * (1 - ((double) (fmin(m_unCollisionsNumber,30.0)/30.0))) * ( (double) (fmin(m_unGreyCounter, 5.0)/ 5.0 )) * redBattery[0]; */
-
-	/* exp5, exp6 */
-  /* double fit = ( m_fComputedFitness / (double) m_unNumberOfSteps ) * (1 - ((double) (fmin(m_unCollisionsNumber,30.0)/30.0))) * ( (double) (fmin(m_unGreyCounter, 5.0)/ 5.0 )) * (fmin(redBattery[0], 0.7)/0.7); */
-
 
   if (m_unGreyFlag == 0 )
     fit /= 10.0;
